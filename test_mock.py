80d7a674c534730e1da52ba56cc054ed5edd4ca8
dcerv005 <dacervantes12@gmail.com>
2024-08-22 02:13:50 -0700

    adjusting tests

--- test_mock.py
+++ test_mock.py
@@ -7,21 +7,21 @@ from services.productService import save
 
 class TestLoginCustomer(unittest.TestCase):
 
-    @patch('services.customerAccountService.db.session.execute') #replacing the db.session.execute with a mock object for testing
-    def test_login_customer(self, mock_customer):
-        #set up the return value for the mock object
-        faker = Faker()
-        mock_user = MagicMock() #simulate a user retrieved from the database
-        mock_user.id = 1
-        mock_user.roles = [MagicMock(role_name='admin'), MagicMock(role_name='user')]
-        password = faker.password()
-        mock_user.username = faker.user_name() #generates a random username
-        mock_user.password = generate_password_hash(password) #generates a random password and hashes it
-        mock_customer.return_value.scalar_one_or_none.return_value = mock_user
+    # @patch('services.customerAccountService.db.session.execute') #replacing the db.session.execute with a mock object for testing
+    # def test_login_customer(self, mock_customer):
+    #     #set up the return value for the mock object
+    #     faker = Faker()
+    #     mock_user = MagicMock() #simulate a user retrieved from the database
+    #     mock_user.id = 1
+    #     mock_user.roles = [MagicMock(role_name='admin'), MagicMock(role_name='user')]
+    #     password = faker.password()
+    #     mock_user.username = faker.user_name() #generates a random username
+    #     mock_user.password = generate_password_hash(password) #generates a random password and hashes it
+    #     mock_customer.return_value.scalar_one_or_none.return_value = mock_user
 
-        response = login_customer(mock_user.username, password)
+    #     response = login_customer(mock_user.username, password)
 
-        self.assertEqual(response['status'], 'success')
+    #     self.assertEqual(response['status'], 'success')
 
     # @patch('services.productService.db.session.execute')
     # def test_post_products(self, mock_product):
